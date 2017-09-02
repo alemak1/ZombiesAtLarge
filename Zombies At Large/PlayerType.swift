461c6571efc0c0fbf3925b37b42b5530f007dbb7
yongrenwang <czie.alexmak@gmail.com>
2017-09-02 16:23:25 +0800

    first commit

---
+++ Zombies At Large/PlayerType.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerType.swift
+//  Zombies At Large
+//
+//  Created by Aleksander Makedonski on 9/1/17.
+//  Copyright © 2017 Aleksander Makedonski. All rights reserved.
+//
+
+import Foundation
